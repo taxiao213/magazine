982ad687ff54ce7778898d9db0b5d6e12760dd35
yin13753884368 <yin13753884368@163.com>
2019-03-18 18:00:26 +0800

    初始化项目

---
+++ app/src/main/java/com/jz/magazine/MainActivity.java
@@ -0,0 +1,21 @@
+package com.jz.magazine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
